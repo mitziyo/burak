29670b0a858d9f000535bd4794de5f471d6e87f9
khan <ziyodullokhan009@mail.ru>
2026-05-23 06:38:43 +0900

    feat: learningabout patterns

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,78 @@
-console.log("*****************");
+// Compiled Language: Java, GoLang, C, C++, C#, Rust  => Compiling & Running
 
-import moment from "moment";
+// INterpreted Languages: NodeJS, Python, PHP, Ruby   => Running
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
+// Error Type
 
-const person: string = "Martin";
-const count: number = 100;
+// Primitive types
+// Object types
+
+// **********************
+// let box: string;
+// box = "hello";
+// box = 100;
+
+// let box: string = "hello";
+// box = 100;
+
+// //***********************
+// const counter: number = 100;
+
+// let stage: number | string = "hello";
+// stage = 100;
+
+// const pending: boolean = true;
+
+//*******************************
+// interface
+// interface Person {
+// 	name: string,
+// 	age: number,
+// 	nation: string
+// }
+
+// const person: Person = {
+// name: 'Martin', 
+// age: 30,
+// nation: 'Australian'
+// }
+
+// interface type uchun hizmat qiladigan soya 
+
+// ********************************************
+// let skills: (number | string)[];
+// let skills: string[];
+// skills = ['Problem Solving', 'Software Design', 'Programming', 100]
+
+
+// class Person {
+// 	age: number;
+// 	firstName: string;
+// 	lastName: string;
+
+// 		constructor(age: number, firstName: string, lastName: string) {
+// 			this.age = age;
+// 			this.firstName = firstName;
+// 			this.lastName = lastName;
+// 			}
+// }
+
+// const person1 = new Person(30, 'Martin', 'Robertson');
+
+
+// console.log("*****************");
+
+// import moment from "moment";
+
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
+
+// const person: string = "Martin";
+// const count: number = 100;
+
+
+// Architectural pattern: MVC, DI, MVP
+// MVC = Model View Controller, MVP = Model View Presentor
+// DI = Dependency injection
+// design pattern: Middleware, Decoretor
+
