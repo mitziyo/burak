30523c27a76cf00fb8df68a89d13a36a21e00d1b
khan <ziyodullokhan009@mail.ru>
2026-05-25 05:02:06 +0900

    feat: completed TASK M

--- train.py
+++ train.py
@@ -1,13 +1,31 @@
+# TASK M
+
+def getSquareNumbers(arr):
+    getNumber = []
+    for element in arr:
+        result = element*element
+        obj_dict = {
+            "number": element,
+            "square": result
+
+        }
+        getNumber.append(obj_dict)
+    return getNumber
+
+
+print(getSquareNumbers([1, 2, 3]))
+
+
 # TASK L
-def reverseSentence(text):
-    words = text.split()
-    result = []
+# def reverseSentence(text):
+#     words = text.split()
+#     result = []
 
-    for element in words:
-        element[::-1]
-        result.append(element)
+#     for element in words:
+#         element[::-1]
+#         result.append(element)
 
-    return " ".join(result)
+#     return " ".join(result)
 
 
-print (reverseSentence("we like cding"))
+# print (reverseSentence("we like cding"))
