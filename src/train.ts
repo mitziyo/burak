89f287a6453eeef06f6b92301d5a771b10afcbf1
khan <ziyodullokhan009@mail.ru>
2026-05-26 17:28:47 +0900

    feat: completed task in Typescript

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,15 @@
-console.log("Hello world")
+// TASK 
+
+function getSquareNumbers(numbers: number[]) {
+    return numbers.map((num) => {
+        return {
+            number: num,
+            square: num*num}
+
+        
+    });
+
+}
+
+const result = getSquareNumbers([1, 2, 3]);
+console.log(result);
