aa6989fcf666a1a9241a277ee89f278fa394894c
khan <ziyodullokhan009@mail.ru>
2026-05-30 05:39:34 +0900

    feat: create router and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1 - ENTRANCE **/
 const app = express();
@@ -9,8 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 /** 2 - SESSION **/
 /** 3 - VIEWS **/
-app.set("views",path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /** 4 - ROUTERS **/
+app.use("/", router); // Middleware design patter
 
 export default app; // modeule.exports = app;
---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router(); // router methodini chaqirb olamz
+import memberController from "./controllers/member.controller";
+
+// router instance dan foydalanib get post methodini amalga oshramz
+router.get("/", memberController.goHome);
+
+router.get("/login", memberController.getLogin);
+
+router.get("/signup", memberController.getSignup);
+
+export default router;
