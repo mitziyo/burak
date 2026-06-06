8fab99873625377a3e0b930783137e12f53462a5
khan <ziyodullokhan009@mail.ru>
2026-06-06 14:39:48 +0900

    feat: fronted related css, js, and image creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -11,11 +11,8 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    // res types => send | json | redirect | end | render
-    // Logic
-    // Service Model
-    // ...
-    res.send("Home Page");
+   
+    res.render("home")
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -24,7 +21,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("Signup Page");
+  res.render("signup")
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
@@ -33,7 +30,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login Page");
+   res.render("login")
   } catch (err) {
     console.log("Error, getLogin:", err);
   }
--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,20 @@ dotenv.config();
 // console.log("PORT:", process.env.PORT);
 
 // console.log("MONGO_URL:", process.env.MONGO_URL);
-    
+
 import mongoose from "mongoose";
 import app from "./app";
 
 mongoose
-.connect(process.env.MONGO_URL as string, {})
-.then((data) => {
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
     console.log("Mongodb connection secceed");
     const PORT = process.env.PORT ?? 3003;
-    app.listen(PORT, function() {
-        console.log(`The server is running successfully on port ${PORT}`);
-    })
-}) 
-.catch((err)=> {
-    console.log("ERROR on connecton MongoDB", err)
-})
+    app.listen(PORT, function () {
+      console.info(`The server is running successfully on port ${PORT}`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR on connecton MongoDB", err);
+  });
