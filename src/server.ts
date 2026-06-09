477e43176a15bd85b71682921936ba645ec42927
khan <ziyodullokhan009@mail.ru>
2026-06-09 15:24:08 +0900

    feat: integrate middleware sessions

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config();
-
-// console.log("PORT:", process.env.PORT);
-
-// console.log("MONGO_URL:", process.env.MONGO_URL);
-
+dotenv.config(); // environmental variablelearni yuklab beradi
 import mongoose from "mongoose";
 import app from "./app";
 
