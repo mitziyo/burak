f777770e9fb6d9a47f337bb305e0997195b8de0b
khan <ziyodullokhan009@mail.ru>
2026-07-04 18:33:55 +0900

    feat: create middleware methods and develop logout process

--- src/router.ts
+++ src/router.ts
@@ -13,6 +13,11 @@ import memberController from "./controllers/member.controller";
 
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
+router.post(
+  "/member/logout",
+  memberController.verifyAuth,
+  memberController.logout,
+);
 router.get("/member/detail", memberController.verifyAuth);
 
 /***** Product *****/
