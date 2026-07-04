c8d5050c23d86784c10aa6cebffbda2eea794596
khan <ziyodullokhan009@mail.ru>
2026-07-04 17:35:54 +0900

    feat: develop verify authenticated user credentials

--- src/router.ts
+++ src/router.ts
@@ -9,8 +9,13 @@ import memberController from "./controllers/member.controller";
 
 // router.get("/signup", memberController.getSignup);
 
-router.post("/login", memberController.login);
+/***** Member *****/
 
-router.post("/signup", memberController.signup);
+router.post("/member/login", memberController.login);
+router.post("/member/signup", memberController.signup);
+router.get("/member/detail", memberController.verifyAuth);
 
+/***** Product *****/
+
+/***** Order *****/
 export default router;
